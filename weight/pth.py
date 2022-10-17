8222d85bfa206ec3a151478d19b017c07f34e3b8
Yimohanser <1067991554@qq.com>
2022-10-17 09:19:03 +0800

    Update Weight

--- weight/pth.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8  -*-
-"""
-Time: 2022/10/16 10:27
-Author: Yimohanser
-Software: PyCharm
-"""
-import torch
-chkpt = torch.load("D:\ML\HST-UNet\weight\Potsdam.pth", map_location='cpu')
-torch.save({"model": chkpt["model"]}, "Potsdam.pth")
